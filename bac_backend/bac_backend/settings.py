27c4ea671f8601faeb63ee77521b933c16dac346
Jamel BEN MEFTEH <jbenmefteh21@gmail.com>
2026-06-10 00:31:09 +0100

    fix: force Python 3.13.5 on Render, set DEBUG=False by default

--- bac_backend/bac_backend/settings.py
+++ bac_backend/bac_backend/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-secret-key-dev')
 
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
